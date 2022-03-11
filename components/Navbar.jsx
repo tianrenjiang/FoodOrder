dc3c3eb87dbb1fb20378113a6de234367a1a6bb6
tianrenjiang <563944616@qq.com>
2022-03-11 10:08:09 +0800

    1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
+import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -12,10 +15,12 @@ const Navbar = () => {
           <div className={styles.text}>ORDER NOW!</div>
           <div className={styles.text}>012 345 678</div>
         </div>
-      </div> {/* 电话号码 */}
+      </div>
       <div className={styles.item}>
         <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
+          <Link href="/" passHref>
+            <li className={styles.listItem}>Homepage</li>
+          </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
           <Image src="/img/logo.png" alt="" width="160px" height="69px" />
@@ -23,13 +28,15 @@ const Navbar = () => {
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
         </ul>
-      </div> {/* 菜单选项 */}
-      <div className={styles.item}>
-        <div className={styles.cart}>
-          <Image src="/img/cart.png" alt="" width="30px" height="30px" />
-          <div className={styles.counter}>2</div>
+      </div>
+      <Link href="/cart" passHref>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src="/img/cart.png" alt="" width="30px" height="30px" />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
         </div>
-      </div>{/* 购物车 */}
+      </Link>
     </div>
   );
 };
